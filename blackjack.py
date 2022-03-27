a602ee5febdfdf76f4485e10c6b4262197278f53
fsanntiago <zerofabricio@hotmail.com>
2022-03-27 12:20:49 -0300

    Add deal the card

--- blackjack.py
+++ blackjack.py
@@ -1,12 +1,21 @@
+import random
+from typing import List
+
+import constants.suits as Suits
+from dealer import Dealer
 from player import Player
 
 
 class Blackjack:
-    def __init__(self, player: Player = None) -> None:
+    def __init__(self, player: Player = None, dealer: Dealer = None) -> None:
         """Initialize Blackjack"""
         self.__player = player
+        self.__dealer = dealer
+        self.__player_hand = []
+        self.__dealer_hand = []
 
         Blackjack.create_player(self)
+        self.__dealer = Dealer()
 
     def play(self) -> bool:
         if self.__player.money == 0:  # Check if the player has run out of money
@@ -17,9 +26,26 @@ class Blackjack:
         print("==============================================")
         self.__player.place_bet()
 
+        # Give the dealer and player two cards from the deck each:
+        deck = Blackjack.get_deck(self)
+        self.__dealer_hand = [(deck.pop(), deck.pop())]
+        self.__player_hand = [(deck.pop(), deck.pop())]
+
     def create_player(self) -> None:
         """Receives player name and create player"""
         name = input("What's your name? \n").strip()
         if not name:
             name = "Player"
         self.__player = Player(name.title())
+
+    def get_deck(self) -> List[tuple]:
+        """Return a list of (rank, suit) tuples for all 52 cards."""
+        deck: List[tuple] = []
+
+        for suit in (Suits.HEARTS, Suits.DIAMONDS, Suits.SPADES, Suits.CLUBS):
+            for rank in range(2, 11):
+                deck.append((str(rank), suit))
+            for rank in ("J", "K", "Q", "A"):
+                deck.append((rank, suit))
+        random.shuffle(deck)
+        return deck
---
+++ constants/suits.py
@@ -0,0 +1,5 @@
+# Set up the constants:
+HEARTS = chr(9829)  # Character 9829 is '♥'.
+DIAMONDS = chr(9830)  # Character 9830 is '♦'.
+SPADES = chr(9824)  # Character 9824 is '♠'.
+CLUBS = chr(9827)  # Character 9827 is '♣'.
